03136407f606610246f6f63049a28fb763ece363
Ben_Ben <benjamin.bouissou@gmail.com>
2023-10-19 18:41:25 +0200

    Final pretty print version

--- GUI.py
+++ GUI.py
@@ -1,4 +1,6 @@
 import board_file
+import utils
+import string
 
 CELL_WIDTH = 3
 CELL_HEIGHT = 3
@@ -10,18 +12,20 @@ class Gui(object):
     def __init__(self):
         pass
     
-    def print_up_border(self):
-        for i in range(WIDTH*5 - 3): # per cell
+    def print_up_border(self, offset = ''):
+        print(offset, end = '')
+        for i in range(WIDTH*4 + 1): # per cell
             if i % 4 == 0:
                 print('+', end = '')
             else:
                 print("-", end = '') 
         print('')
     
-    def print_middle_cell(self, row):
+    def print_middle_cell(self, row, nbr_row):
         j = 0
         #print('A', end = '')
-        for i in range(WIDTH*5 - 2):
+        print(f' {nbr_row} ', end = '')
+        for i in range(WIDTH*4 + 1):
             if i % 4 == 0:
                 print('|', end = '')
             elif i%2==0 and i%4 !=0 and j<len(row):
@@ -31,12 +35,27 @@ class Gui(object):
             else:
                 print(' ', end = '')
         print('')
-        
+    def print_letter_up(self, offset=''):
+        print('   ')
+        print(offset, end = '')
+        j = 0
+        for i in range(WIDTH*4 + 1):
+            if i%2==0 and i%4 !=0:
+                print(string.ascii_uppercase[j], end = '')
+                j+=1
+            else:
+                print(' ', end = '')
+        print('')
+            
+
     def Pretty_print(self, Board):
-        self.print_up_border()
-        for row in Board:
-            self.print_middle_cell(row)
-            self.print_up_border()
+        offset = '   '
+        self.print_letter_up(offset)
+        self.print_up_border(offset)
+        
+        for idr, row in enumerate(Board):
+            self.print_middle_cell(row, idr+1)
+            self.print_up_border(offset)
 
 
 if __name__ == '__main__':
